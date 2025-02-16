83226eff4f7ecd73dbb4726509c96261f6294cea
Bpost129 <Bpost129@yahoo.com>
2025-02-16 00:02:11 -0500

    give each ingredient its associated background color

--- src/Components/BurgerShop/Ingredient.jsx
+++ src/Components/BurgerShop/Ingredient.jsx
@@ -1,7 +1,7 @@
 
 const Ingredient = (props) => {
 	return (
-		<li>
+		<li style={{backgroundColor: props.ingredient.color}}>
 			<p>{props.ingredient.name}</p>
 			<button onClick={() => props.addToBurger(props.ingredient)}>+</button>
 			<button onClick={() => props.removeFromBurger(props.idx)}>X</button>
