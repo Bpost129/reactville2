adfbd8fe187e3da7853f3ab5298befe5029f77f8
Bpost129 <Bpost129@yahoo.com>
2025-02-16 00:00:31 -0500

    render message when burger stack is empty

--- src/Components/BurgerShop/BurgerStack.jsx
+++ src/Components/BurgerShop/BurgerStack.jsx
@@ -1,6 +1,8 @@
 import Ingredient from "./Ingredient"
 
 const BurgerStack = (props) => {
+  if (!props.stack.length) return <ul className="burger-stack" style={{textAlign: 'center'}}>No ingredients yet</ul>
+
   return (
     <ul className="burger-stack">
       {props.stack.map((item, idx) =>
