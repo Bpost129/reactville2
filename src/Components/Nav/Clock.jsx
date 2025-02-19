5c2a91ec76db6bab5c79db0258c2c6af541f7dd9
Bpost129 <Bpost129@yahoo.com>
2025-02-18 23:24:35 -0500

    import and add clock module to navbar, create state and time variables for updating the clock

--- src/Components/Nav/Clock.jsx
+++ src/Components/Nav/Clock.jsx
@@ -1,5 +1,11 @@
+import { useState, useEffect } from "react"
+
+import { getMinutes } from "../../modules/functions"
 
 const Clock = () => {
+  const [date, setDate] = useState(new Date())
+  const time = date.toLocaleTimeString()
+
   return (
     <p className="display-items">
       5:00 PM
