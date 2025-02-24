f1a93fb464ddce260e3ded23446cab22909a6e30
Bpost129 <Bpost129@yahoo.com>
2025-02-23 23:49:45 -0500

    render added items to cart

--- src/Components/SuperMarket/CartItem.jsx
+++ src/Components/SuperMarket/CartItem.jsx
@@ -1,12 +1,12 @@
 
-const CartItem = (props) => {
+const CartItem = ({ item }) => {
 	return (
 		<div className="cart-card">
-			<img src="" alt="product icon" />
+			<img src={item.image} alt="product icon" />
 			<span>
-				<p id="product-name">{props.item.name}</p>
-				<p id="product-price">${props.item.price}</p>
-				<p id="product-quantity">Quantity:{props.item.quantity}</p>
+				<p id="product-name">{item.name}</p>
+				<p id="product-price">${item.price}</p>
+				<p id="product-quantity">Quantity:{item.quantity}</p>
 			</span>
 			<button>X</button>
 		</div>
