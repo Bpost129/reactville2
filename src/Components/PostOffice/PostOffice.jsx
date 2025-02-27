988d6ab0478233a96dc468901b8ccd67f3121ac7
Bpost129 <Bpost129@yahoo.com>
2025-02-26 23:55:30 -0500

    create descendant routes within post office component, import associated components

--- src/Components/PostOffice/PostOffice.jsx
+++ src/Components/PostOffice/PostOffice.jsx
@@ -1,6 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+
 import '../../styles/mail.css'
 
 //Data & Assets
+import BoxList from './BoxList'
+import BoxDetails from './BoxDetails'
+import NewLetter from './NewLetter/NewLetter'
+import NewBox from './NewBox/NewBox'
+
 import LobbyImg from '../../assets/post-office-lobby.png'
 import { initialPOBoxes, initialLetters } from "../../data/post-data"
 
@@ -17,7 +24,24 @@ const PostOffice = (props) => {
 
 			<div className="postoffice-layout">
 				<section className="img-container"><img src={LobbyImg} alt="post office illustration" /></section>
-				Routes here
+				<Routes>
+				<Route
+            path='/'
+            element={<BoxList />}
+          />
+          <Route 
+            path='/:boxNo'
+            element={<BoxDetails />}
+          />
+          <Route 
+            path='/letters/new'
+            element={<NewLetter />}
+          />
+          <Route 
+            path='/boxes/new'
+            element={<NewBox />}
+          />
+				</Routes>
 			</div>
 
 		</div>
