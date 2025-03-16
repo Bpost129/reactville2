71392b0c0e9eb20ea01621d1aaba88126a3aa25f
Bpost129 <Bpost129@yahoo.com>
2025-03-15 21:33:20 -0400

    add error message and back button to url of nonexistent boxes

--- src/Components/PostOffice/BoxDetails.jsx
+++ src/Components/PostOffice/BoxDetails.jsx
@@ -1,25 +1,34 @@
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 
 import LetterList from './LetterList'
 
 const BoxDetails = ({ boxes, letters, markAsRead }) => {
+  const navigate = useNavigate()
   const { boxNo } = useParams()
   const poBox = boxes[boxNo]
 
-  return (
-    <section className="box-details">
-      <header>
-        <h3>PO Box {boxNo}</h3>
-        <select>
-          {poBox.boxHolders.map((holder, idx) => 
-            <option key={idx} value={holder}>{holder}</option>
-          )}
-        </select>
-      </header>
-      <LetterList letters={letters} letterIds={poBox.letters} markAsRead={markAsRead} />
-
-    </section>
-  )
+  if (poBox) {
+    return (
+      <section className="box-details">
+        <header>
+          <h3>PO Box {boxNo}</h3>
+          <select>
+            {poBox.boxHolders.map((holder, idx) => 
+              <option key={idx} value={holder}>{holder}</option>
+            )}
+          </select>
+        </header>
+        <LetterList letters={letters} letterIds={poBox.letters} markAsRead={markAsRead} />
+      </section>
+    )
+  } else {
+    return (
+      <div>
+        <h4>Hmm, this box doesn't seem to exist.</h4>
+        <button onClick={() => navigate('/postoffice')}>Return To Lobby</button>
+      </div>
+    )
+  }
 }
 
 export default BoxDetails
