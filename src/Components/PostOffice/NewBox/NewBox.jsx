233b057f698498983cf656718141b26f904dbfc2
Bpost129 <Bpost129@yahoo.com>
2025-03-18 22:05:59 -0400

    render message confirming po box creating before navigating to lobby

--- src/Components/PostOffice/NewBox/NewBox.jsx
+++ src/Components/PostOffice/NewBox/NewBox.jsx
@@ -8,6 +8,7 @@ const NewBox = ({ createBox }) => {
   const [boxHolders, setBoxHolders] = useState([])
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
+  const [status, setStatus] = useState('')
   const fullName = firstName + " " + lastName
   const costOfBox = (10 * boxHolders.length)
 
@@ -33,33 +34,42 @@ const NewBox = ({ createBox }) => {
   }
   
   const handleRedirect = () => {
-    navigate('/postoffice')
+    setTimeout(() => navigate('/postoffice'), 2000)
+    
   }
 
   const handleSubmit = () => {
     createBox(boxHolders, costOfBox)
+    setStatus('Your PO Box has been created')
     handleRedirect()
   }
 
+  if (status) {
+    return (
+      <div className="status-msg">
+        <h4>{status}</h4>
+      </div>
+    )
+  } else {
+    return (
+      <section className="post-office-form">
 
-  return (
-    <section className="post-office-form">
+        <header>
+          <h3>New PO Box</h3>
+          <p>Total: ${costOfBox}</p>
+          <button id="submit-box-btn" disabled={!boxHolders.length} onClick={() => handleSubmit()}>SUBMIT</button>
+        </header>
 
-      <header>
-        <h3>New PO Box</h3>
-        <p>Total: ${costOfBox}</p>
-        <button id="submit-box-btn" disabled={!boxHolders.length} onClick={() => handleSubmit()}>SUBMIT</button>
-      </header>
+        <section>
+          <BoxHolders boxHolders={boxHolders} removeBoxHolder={removeBoxHolder} />
+          <input placeholder="First Name" type="text" name="firstName" value={firstName} onChange={handleChange} />
+          <input placeholder="Last Name" type="text" name="lastName" value={lastName} onChange={handleChange} />
+          <button id="add-boxholder" disabled={!firstName || !lastName} onClick={() => addBoxHolder()}>ADD BOXHOLDER</button>
+        </section>
 
-      <section>
-        <BoxHolders boxHolders={boxHolders} removeBoxHolder={removeBoxHolder} />
-        <input placeholder="First Name" type="text" name="firstName" value={firstName} onChange={handleChange} />
-        <input placeholder="Last Name" type="text" name="lastName" value={lastName} onChange={handleChange} />
-        <button id="add-boxholder" disabled={!firstName || !lastName} onClick={() => addBoxHolder()}>ADD BOXHOLDER</button>
       </section>
-
-    </section>
-  )
+    )
+  }
 }
 
 export default NewBox
